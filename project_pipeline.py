55aa6ac75f31998308a5075a14bc7bd1d9d9ffa0
Kaylocat <amartinez28465@gmail.com>
2026-03-19 22:41:31 -0500

    Added Lineage

--- project_pipeline.py
+++ project_pipeline.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from pathlib import Path
 import logging
+from datetime import datetime
+from dataclasses import dataclass
 
 import clean_data as cd
 import genres as genre
@@ -20,6 +22,16 @@ logger = logging.getLogger("Project Logger")
 COLUMNS_TO_REMOVE = ["alternative_title", "mal_url", "image_url", "genres_detailed"]
 NUMERIC_COLUMNS = ["year", "episodes", "score"]
 NON_NULLABLE_COLUMNS = ["title", "type", "year", "score"]
+Lineage = []
+
+@dataclass
+class lineage_event:
+    action: str
+    result: str
+    timestamp: str
+
+def now_iso():
+    return datetime.utcnow().isoformat(timespec="seconds")
 
 # ------ Load Data -------
 def load_data(path: Path) -> pd.DataFrame:
@@ -28,28 +40,107 @@ def load_data(path: Path) -> pd.DataFrame:
     Returns resulting dataframe.
     """
     df = pd.read_csv(path)
-    logger.info("\nRead " + str(df.shape[0]) + " rows from " + str(path) + "\n")
+    message = "Read " + str(df.shape[0]) + " rows from " + str(path)
+    logger.info("\n" + message + "\n")
+    Lineage.append(lineage_event(
+        action="Load data from CSV file at " + str(path),
+        result=message,
+        timestamp=now_iso()
+    ))
+
     return df
 
 def create_tables():
     """Create tables for raw data, genres, and relationship table."""
-    logger.info("\n" + str(db.create_raw_table()) + "\n")
-    logger.info("\n" + str(db.create_genres_table()) + "\n")
-    logger.info("\n" + str(db.create_anime_genres_table()) + "\n")
-    logger.info("\n" + str(db.create_type_tables()) + "\n")
+    message = str(db.create_raw_table())
+    logger.info("\n" + message + "\n")
+    Lineage.append(lineage_event(
+        action="Create raw data table.",
+        result=message,
+        timestamp=now_iso()
+    ))
+    
+    message = str(db.create_genres_table())
+    logger.info("\n" + message + "\n")
+    Lineage.append(lineage_event(
+        action="Create genres table.",
+        result=message,
+        timestamp=now_iso()
+    ))
+
+    message = str(db.create_anime_genres_table())
+    logger.info("\n" + message + "\n")
+    Lineage.append(lineage_event(
+        action="Create genres by anime table.",
+        result=message,
+        timestamp=now_iso()
+    ))
+
+    message = str(db.create_type_tables())
+    logger.info("\n" + message + "\n")
+    Lineage.append(lineage_event(
+        action="Create tables for series and movies.",
+        result=message,
+        timestamp=now_iso()
+    ))
 
 def input_data(df: pd.DataFrame, genres: list, genres_by_row: dict):
     """Insert data into raw data, genres, and relationship table."""
     logger.info("\nBegin insert into ANIME_RAW.\n")
-    logger.info("\n" + str(db.input_raw_data(df)) + "\n")
+    Lineage.append(lineage_event(
+        action="Insert data",
+        result="Begin insert into ANIME_RAW.",
+        timestamp=now_iso()
+    ))
+    message = str(db.input_raw_data(df))
+    logger.info("\n" + message + "\n")
+    Lineage.append(lineage_event(
+        action="Insert data",
+        result=message,
+        timestamp=now_iso()
+    ))
 
     logger.info("\nBegin insert into GENRES.")
-    logger.info("\n" + str(db.input_genres(genres)) + "\n")
+    Lineage.append(lineage_event(
+        action="Insert data",
+        result="Begin insert into GENRES.",
+        timestamp=now_iso()
+    ))
+    message = str(db.input_genres(genres))
+    logger.info("\n" + message + "\n")
+    Lineage.append(lineage_event(
+        action="Insert data",
+        result=message,
+        timestamp=now_iso()
+    ))
 
     logger.info("\nBegin insert into GENRES_BY_ANIME.\n")
-    logger.info("\n" + str(db.input_genres_by_anime(genres_by_row)) + "\n")
-    db.input_type_tables()
-    logger.info("\n" + str(db.input_type_tables()) + "\n")
+    Lineage.append(lineage_event(
+        action="Insert data",
+        result="Begin insert into GENRES_BY_ANIME.",
+        timestamp=now_iso()
+    ))
+    message = str(db.input_genres_by_anime(genres_by_row))
+    logger.info("\n" + message + "\n")
+    Lineage.append(lineage_event(
+        action="Insert data",
+        result=message,
+        timestamp=now_iso()
+    ))
+
+    logger.info("\nBegin insert into MOVIES and SERIES.\n")
+    Lineage.append(lineage_event(
+        action="Insert data",
+        result="Begin insert into MOVIES and SERIES.",
+        timestamp=now_iso()
+    ))
+    message = str(db.input_type_tables())
+    logger.info("\n" + message + "\n")
+    Lineage.append(lineage_event(
+        action="Insert data",
+        result=message,
+        timestamp=now_iso()
+    ))
 
 # ------ Transformations ------
 def clean_dataframe(df: pd.DataFrame) -> pd.DataFrame:
@@ -63,18 +154,35 @@ def clean_dataframe(df: pd.DataFrame) -> pd.DataFrame:
     df = cd.cast_numbers(df, NUMERIC_COLUMNS)
     df = cd.remove_null_rows(df, NON_NULLABLE_COLUMNS)
 
-    logger.info(f"\nRemoved {initial_rows - df.shape[0]} invalid rows from Dataframe.\n")
+    message = f"Removed {initial_rows - df.shape[0]} invalid rows from Dataframe."
+    logger.info("\n" + message + "\n")
+    Lineage.append(lineage_event(
+        action="Transformations",
+        result=message,
+        timestamp=now_iso()
+    ))
+
     return df
 
 # ------ Analysis -------
-def create_graphs():
+def create_graphs(path):
     genres_table = an.get_score_by_genre()
-    graph.plot_genres_bar(genres_table, OUTDIR / "Score_by_genre")
-    year_table = an.get_score_by_year()
-    graph.plot_year_bar(year_table, OUTDIR / "Score_by_year")
-    #print(year_table)
+    graph.plot_genres_bar(genres_table, path / "Score_by_genre")
+    Lineage.append(lineage_event(
+        action="Analysis",
+        result="Created image Score_by_genre in " + str(path),
+        timestamp=now_iso()
+    ))
 
-# ------ Orchestration ------
+    year_table = an.get_score_by_year()
+    graph.plot_year_bar(year_table, path / "Score_by_year")
+    Lineage.append(lineage_event(
+        action="Analysis",
+        result="Created image Score_by_year in " + str(path),
+        timestamp=now_iso()
+    ))
+
+# ------ Execute Pipeline ------
 def main():
     df = load_data(RAWDATA)
 
@@ -87,8 +195,11 @@ def main():
     create_tables()
     input_data(df, genres, genres_by_row)
 
-    #print(df.head())
-    create_graphs()
+    create_graphs(OUTDIR)
+
+    with open(OUTDIR / "logs.txt", "w", encoding="UTF-8") as log_file:
+        for _event in Lineage:
+            log_file.write(f"{_event.timestamp}\n{_event.action}\n\t Result: {_event.result}\n\n")
 
 if __name__ == "__main__":
     main()
